330bfdd5be2e354b7062de4b3f2fb6a71ff0da50
Quzeem <quzeemagbaje@gmail.com>
2021-07-30 13:21:51 +0100

    Graceful shutdown of NATS client

--- tickets/src/config/nats.ts
+++ tickets/src/config/nats.ts
@@ -3,6 +3,13 @@ import { natsWrapper } from './natsWrapper';
 export const connectNATS = async () => {
   try {
     await natsWrapper.connect('zeeticket', 'abc', 'http://nats-srv:4222');
+    // Graceful Shutdown - Gotcha => code like this(which can cause our entire program to exit) should not be put/hidden away inside of some method in a class/far away file(bad design!). we only ever want to allow something to exit our entire program from inside of more central location such as the index.ts file(This is good since we're still calling this function in our index.ts file - same as writing this code in index.ts file)
+    natsWrapper.client.on('close', () => {
+      console.log('NATS connection closed');
+      process.exit();
+    });
+    process.on('SIGINT', () => natsWrapper.client.close()); // Listen for interrupt signals
+    process.on('SIGTERM', () => natsWrapper.client.close()); // Listen for terminate signals
   } catch (err) {
     console.error(err);
   }
