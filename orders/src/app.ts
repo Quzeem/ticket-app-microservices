d1cfac85651e92fb94206365ac60d6e954ba603e
Quzeem <quzeemagbaje@gmail.com>
2021-08-03 16:44:09 +0100

    Orders service initial setup

---
+++ orders/src/app.ts
@@ -0,0 +1,28 @@
+import express, { Request, Response } from 'express';
+import 'express-async-errors';
+import cookieSession from 'cookie-session';
+
+import { errorHandler, NotFoundError, setCurrentUser } from '@zeetickets/lib';
+
+const app = express();
+
+// Traffic is being proxy to our app through ingress-nginx. This is to make express trust the traffic as being secure.
+app.set('trust proxy', true); // app.enable('trust proxy')
+app.use(express.json());
+app.use(
+  cookieSession({
+    signed: false,
+    secure: process.env.NODE_ENV !== 'test',
+    httpOnly: true,
+  })
+);
+app.use(setCurrentUser);
+
+app.all('*', async (req: Request, res: Response) => {
+  throw new NotFoundError(
+    `${req.method} request to: ${req.originalUrl} not available on this server!`
+  );
+});
+app.use(errorHandler);
+
+export { app };
---
+++ orders/src/index.ts
@@ -0,0 +1,26 @@
+import { app } from './app';
+import { connectDB } from './config/db';
+import { connectNATS } from './config/nats';
+
+if (!process.env.JWT_SECRET) {
+  throw new Error('JWT_SECRET must be defined');
+}
+if (!process.env.MONGO_URI) {
+  throw new Error('MONGO_URI must be defined');
+}
+if (!process.env.NATS_CLIENT_ID) {
+  throw new Error('NATS_CLIENT_ID must be defined');
+}
+if (!process.env.NATS_URL) {
+  throw new Error('NATS_URL must be defined');
+}
+if (!process.env.NATS_CLUSTER_ID) {
+  throw new Error('NATS_CLUSTER_ID must be defined');
+}
+
+// Connect to MongoDB
+connectDB();
+// Connect to NATS
+connectNATS();
+
+app.listen(3000, () => console.log('Server listening on port 3000'));
